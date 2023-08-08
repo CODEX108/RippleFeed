3bc829c971d1fc6fa0e42f2b51a27d20aeabe14e
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-08-08 20:37:49 +0530

    added comment functionality

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,55 +7,61 @@ import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
-    placeholder: string,
-    iscomment?: boolean,
-    postId?: string
+  placeholder: string,
+  isComment?: boolean,
+  postId?: string
 }
 const Form: React.FC<FormProps> = ({
-    placeholder,
-    iscomment,
-    postId
+  placeholder,
+  isComment,
+  postId
 }) => {
 
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
 
-    const { data: currentUser } = useCurrentUser();
-    const { mutate: mutatePosts } = usePosts();
-    const [body, setBody] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutatePosts } = usePosts();
+  const {mutate:mutatePost} = usePost(postId as string);
+  const [body, setBody] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    const onSubmit = useCallback(async () => {
-        try {
-            setIsLoading(true);
-            await axios.post('/api/posts', { body });
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
 
-            toast.success('Ripple Created');
-            setBody('');
-            //mutate existing post
-            mutatePosts();
-        } catch (error) {
-            console.log(error);
-            toast.error('Something went wrong!');
-        } finally {
-            setIsLoading(false);
-        }
-    }, [body,mutatePosts]);
-    return (
-        <div className="border-b-[1px] border-neutral-800 px-5 py-2">
-          {currentUser ? (
-            <div className="flex flex-row gap-4">
-              <div>
-                <Avatar userId={currentUser?.id} />
-              </div>
-              <div className="w-full">
-                <textarea
-                  disabled={isLoading}
-                  onChange={(event) => setBody(event.target.value)}
-                  value={body}
-                  className="
+      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
+
+      await axios.post(url, { body });
+
+      toast.success('Ripple Created');
+      setBody('');
+      //mutate existing post
+      mutatePosts();
+      mutatePost();
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong!');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [body, mutatePosts, isComment, postId,mutatePost]);
+  return (
+    <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+      {currentUser ? (
+        <div className="flex flex-row gap-4">
+          <div>
+            <Avatar userId={currentUser?.id} />
+          </div>
+          <div className="w-full">
+            <textarea
+              disabled={isLoading}
+              onChange={(event) => setBody(event.target.value)}
+              value={body}
+              className="
                     disabled:opacity-80
                     peer
                     resize-none 
@@ -68,33 +74,33 @@ const Form: React.FC<FormProps> = ({
                     placeholder-neutral-500 
                     text-white
                   "
-                  placeholder={placeholder}>
-                </textarea>
-                <hr 
-                  className="
+              placeholder={placeholder}>
+            </textarea>
+            <hr
+              className="
                     opacity-0 
                     peer-focus:opacity-100 
                     h-[1px] 
                     w-full 
                     border-neutral-800 
                     transition"
-                />
-                <div className="mt-4 flex flex-row justify-end">
-                  <Button disabled={isLoading || !body} onClick={onSubmit} label="Drop" />
-                </div>
-              </div>
+            />
+            <div className="mt-4 flex flex-row justify-end">
+              <Button disabled={isLoading || !body} onClick={onSubmit} label="Drop" />
             </div>
-          ) : (
-            <div className="py-8">
-              <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to RippleFeed</h1>
-              <div className="flex flex-row items-center justify-center gap-4">
-                <Button label="Login" onClick={loginModal.onOpen} />
-                <Button label="Register" onClick={registerModal.onOpen} secondary />
-              </div>
-            </div>
-          )}
+          </div>
         </div>
-      );
+      ) : (
+        <div className="py-8">
+          <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to RippleFeed</h1>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <Button label="Login" onClick={loginModal.onOpen} />
+            <Button label="Register" onClick={registerModal.onOpen} secondary />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Form;
--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,39 +1,36 @@
 import { useRouter } from "next/router";
-import { ClipLoader, PuffLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 
 import usePost from "@/hooks/usePost";
-import Header from "@/components/Header";
-import PostItem from "@/components/posts/PostItem";
-import Form from "@/components/Form";
 
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import PostItem from "@/components/posts/PostItem";
+import CommentFeed from "@/components/posts/CommentFeed";
 
 
 const PostView = () => {
-    const router = useRouter();
-    const {postId} = router.query;
+  const router = useRouter();
+  const { postId } = router.query;
 
-    const {data:fetchedPost,isLoading} = usePost(postId as string);
+  const { data: fetchedPost, isLoading } = usePost(postId as string);
 
-    if(isLoading||!fetchedPost){
-        return(
-            <div className="flex justify-center items-center h-screen">
-                <PuffLoader color="#6146e8" size={150} />
-            </div>
-        )
-    }
- return(
-      <>
-      <Header label="Drop" showBackArrow/>
-      <PostItem
-      data={fetchedPost}
-      />
-      <Form 
-      postId={postId as string}
-      iscomment
-      placeholder="Create a ripple"
-      />
-      </>
- )
+  if (isLoading || !fetchedPost) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    )
+  }
+
+  return ( 
+    <>
+      <Header showBackArrow label="Drop" />
+      <PostItem data={fetchedPost} />
+      <Form postId={postId as string} isComment placeholder="Create a ripple" />
+      <CommentFeed comments={fetchedPost?.comments} />
+    </>
+   );
 }
  
 export default PostView;
